7fdb5f8b8d77ec9853754fc1a2efa9a1724cdd90
Matthew McConnell <maca134@googlemail.com>
2025-09-23 12:49:05 +0100

    refactor: simplify formatting in sepBy1 function return statement

--- src/combinators/sepBy1.ts
+++ src/combinators/sepBy1.ts
@@ -6,8 +6,7 @@ export function sepBy1<T, S>(
 	parser: Percy<T>,
 	separator: Percy<S>,
 ): Percy<T[]> {
-	return seq(parser, many(seq(separator, parser).map(([_, p]) => p))).map(([first, rest]) => [
-		first,
-		...rest,
-	]);
+	return seq(parser, many(seq(separator, parser).map(([_, p]) => p))).map(
+		([first, rest]) => [first, ...rest],
+	);
 }
